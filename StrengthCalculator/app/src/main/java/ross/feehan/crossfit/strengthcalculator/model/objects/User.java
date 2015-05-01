3739a645be02fd751e803dbf1d6a7d50553e9c7b
rossfeehan <thafirstboss@yahoo.co.uk>
2015-05-01 15:43:37 +0100

    Created User Realm Object

---
+++ StrengthCalculator/app/src/main/java/ross/feehan/crossfit/strengthcalculator/model/objects/User.java
@@ -0,0 +1,35 @@
+package ross.feehan.crossfit.strengthcalculator.model.objects;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by Ross Feehan on 01/05/2015.
+ * Copyright Ross Feehan
+ */
+public class User extends RealmObject {
+
+    private String preferedUnits, sex;
+
+    //CONSTRUCTORS
+    public User(){
+
+    }
+
+    //SETTERS
+    public void setPreferedUnits(String preferedUnits) {
+        this.preferedUnits = preferedUnits;
+    }
+    
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    //GETTERS
+    public String getPreferedUnits() {
+        return preferedUnits;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+}
