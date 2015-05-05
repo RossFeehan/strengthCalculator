a38f8a1155f65ade79176598a3390fa99e9910cf
rossfeehan <thafirstboss@yahoo.co.uk>
2015-05-05 16:11:22 +0100

    Created UI for Welcome/ Set up user screen

--- StrengthCalculator/app/src/main/java/ross/feehan/crossfit/strengthcalculator/view/SetUpUserDetails.java
+++ StrengthCalculator/app/src/main/java/ross/feehan/crossfit/strengthcalculator/view/SetUpUserDetails.java
@@ -1,10 +1,17 @@
 package ross.feehan.crossfit.strengthcalculator.view;
 
+
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
+import javax.inject.Inject;
+
+import butterknife.InjectView;
 import ross.feehan.crossfit.strengthcalculator.R;
+import ross.feehan.crossfit.strengthcalculator.model.objects.User;
 
 /**
  * Created by Ross Feehan on 05/05/2015.
@@ -13,11 +20,22 @@ import ross.feehan.crossfit.strengthcalculator.R;
 public class SetUpUserDetails extends Activity {
 
     private Context ctx;
+    @InjectView(R.id.lbsRBTN) RadioButton lbsRadioBTN;
+    @InjectView(R.id.kgsRBTN)RadioButton kgsRadioBTN;
+    @InjectView(R.id.maleRBTN)RadioButton maleRadioBTN;
+    @InjectView(R.id.femaleRBTN) RadioButton femaleRadioBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_up_user_activity);
+
+        ActionBar actionBar = getActionBar();
+
+        if(actionBar != null){
+            actionBar.hide();
+        }
+
 
     }
 
