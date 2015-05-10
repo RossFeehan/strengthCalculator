19f689e6c83002e0a9a2a04d3aaf4fe4ee4f4ed5
rossfeehan <thafirstboss@yahoo.co.uk>
2015-05-10 19:47:56 +0100

    Added more UI Espresso test for New Calcualtions UI

--- StrengthCalculator/app/src/androidTest/java/ross/feehan/crossfit/strengthcalculator/viewTests/NewCalculations_ActivityTests.java
+++ StrengthCalculator/app/src/androidTest/java/ross/feehan/crossfit/strengthcalculator/viewTests/NewCalculations_ActivityTests.java
@@ -33,9 +33,18 @@ public class NewCalculations_ActivityTests {
     @Rule
     public ActivityTestRule activityRule = new ActivityTestRule(NewCalculations_Activity.class);
 
-    //Test to check that everything is displayed correctly on screen when opened
     @Test
-    public void checkInitialLayout(){
+    public void checkThatAllCardLayoutsAreDisplayed(){
+
+        onView(withId(R.id.benchPressCV)).check(matches(isDisplayed()));
+        onView(withId(R.id.squatCV)).check(matches(isDisplayed()));
+        onView(withId(R.id.deadLiftCV)).check(matches(isDisplayed()));
+        onView(withId(R.id.overHeadPressCV)).check(matches(isDisplayed()));
+    }
+
+    //Test to check that everything is displayed correctly in Bench Press Card View
+    @Test
+    public void checkBenchPressCardView(){
 
         //both Edit Texts are visible
         onView(withId(R.id.benchPressRepsET)).check(matches(isDisplayed()));
@@ -50,8 +59,51 @@ public class NewCalculations_ActivityTests {
         onView(withId(R.id.benchPressWeightTV)).check(matches(not(isDisplayed())));
     }
 
+    //Test to check that everything is displayed correctly in squat Card View
     @Test
-    public void checkTextViewsDisplayWhenWeightETHasFocus(){
+    public void checkSquatCardView(){
+
+        //both Edit Texts are visible
+        onView(withId(R.id.squatRepsET)).check(matches(isDisplayed()));
+        onView(withId(R.id.squatWeightET)).check(matches(isDisplayed()));
+
+        //all text views relating to Edit Texts are hidden
+        onView(withId(R.id.squatRepsTV)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.squatForTV)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.squatWeightTV)).check(matches(not(isDisplayed())));
+    }
+
+    //Test to check that everything is displayed correctly in deadLift Card View
+    @Test
+    public void checkDeadLiftCardView(){
+
+        //both Edit Texts are visible
+        onView(withId(R.id.deadLiftRepsET)).check(matches(isDisplayed()));
+        onView(withId(R.id.deadLiftWeightET)).check(matches(isDisplayed()));
+
+        //all text views relating to Edit Texts are hidden
+        onView(withId(R.id.deadLiftRepsTV)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.deadLiftForTV)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.deadLiftWeightTV)).check(matches(not(isDisplayed())));
+    }
+
+    //Test to check that everything is displayed correctly in over Head Press Card View
+    @Test
+    public void checkOverHeadPressCardView(){
+
+        //both Edit Texts are visible
+        onView(withId(R.id.overHeadPressRepsET)).check(matches(isDisplayed()));
+        onView(withId(R.id.overHeadPressWeightET)).check(matches(isDisplayed()));
+
+        //all text views relating to Edit Texts are hidden
+        onView(withId(R.id.overHeadPressRepsTV)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.overHeadPressForTV)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.overHeadPressWeightTV)).check(matches(not(isDisplayed())));
+    }
+
+    //test for when Bench Press Weight Edit Text has focus
+    @Test
+    public void checkTextViewsDisplayWhenBenchPressWeightETHasFocus(){
 
         //when weight Edit Text has focus
         onView(withId(R.id.benchPressWeightET)).perform(click()).check(matches(hasFocus()));
@@ -64,8 +116,55 @@ public class NewCalculations_ActivityTests {
         onView(withId(R.id.benchPressWeightTV)).check(matches(not(isDisplayed())));
     }
 
+    //test for when Squat Weight Edit Text has focus
     @Test
-    public void enterTextIntoWeightEt(){
+    public void checkTextViewsDisplayWhenSquatWeightETHasFocus(){
+
+        //when weight Edit Text has focus
+        onView(withId(R.id.squatWeightET)).perform(click()).check(matches(hasFocus()));
+
+        //the text views are displayed
+        onView(withId(R.id.squatRepsTV)).check(matches(isDisplayed()));
+        onView(withId(R.id.squatForTV)).check(matches(isDisplayed()));
+
+        //the weight unit is not displayed
+        onView(withId(R.id.squatWeightTV)).check(matches(not(isDisplayed())));
+    }
+
+    //test for when Dead lift Weight Edit Text has focus
+    @Test
+    public void checkTextViewsDisplayWhenDeadLiftWeightETHasFocus(){
+
+        //when weight Edit Text has focus
+        onView(withId(R.id.deadLiftWeightET)).perform(click()).check(matches(hasFocus()));
+
+        //the text views are displayed
+        onView(withId(R.id.deadLiftRepsTV)).check(matches(isDisplayed()));
+        onView(withId(R.id.deadLiftForTV)).check(matches(isDisplayed()));
+
+        //the weight unit is not displayed
+        onView(withId(R.id.deadLiftWeightTV)).check(matches(not(isDisplayed())));
+    }
+
+    //test for when Over Head Press Weight Edit Text has focus
+    @Test
+    public void checkTextViewsDisplayWhenOverHeadPressWeightETHasFocus(){
+
+        //when weight Edit Text has focus
+        onView(withId(R.id.overHeadPressWeightET)).perform(click()).check(matches(hasFocus()));
+
+        //the text views are displayed
+        onView(withId(R.id.overHeadPressRepsTV)).check(matches(isDisplayed()));
+        onView(withId(R.id.overHeadPressForTV)).check(matches(isDisplayed()));
+
+        //the weight unit is not displayed
+        onView(withId(R.id.overHeadPressWeightTV)).check(matches(not(isDisplayed())));
+    }
+
+    //test to check that weight tv is displayed after something is entered into weight edit text
+    //in bench press card view
+    @Test
+    public void enterTextIntoBenchPressWeightEt(){
 
         final int WEIGHTTEXT = 60;
 
@@ -76,5 +175,53 @@ public class NewCalculations_ActivityTests {
 
         //check to see the weight unit text is now visible
         onView(withId(R.id.benchPressWeightTV)).check(matches(isDisplayed()));
+    }
+
+    //test to check that weight tv is displayed after something is entered into weight edit text
+    //in squat card view
+    @Test
+    public void enterTextIntoSquatWeightEt(){
+
+        final int WEIGHTTEXT = 60;
+
+        //click the weight Edit Text
+        onView(withId(R.id.squatWeightET)).perform(click()).check(matches(hasFocus()));
+        //enter text into weight Edit Text
+        onView(withId(R.id.squatWeightET)).perform(typeText(String.valueOf(WEIGHTTEXT)));
+
+        //check to see the weight unit text is now visible
+        onView(withId(R.id.squatWeightTV)).check(matches(isDisplayed()));
+    }
+
+    //test to check that weight tv is displayed after something is entered into weight edit text
+    //in Dead Lift card view
+    @Test
+    public void enterTextIntoDeadLiftWeightEt(){
+
+        final int WEIGHTTEXT = 60;
+
+        //click the weight Edit Text
+        onView(withId(R.id.deadLiftWeightET)).perform(click()).check(matches(hasFocus()));
+        //enter text into weight Edit Text
+        onView(withId(R.id.deadLiftWeightET)).perform(typeText(String.valueOf(WEIGHTTEXT)));
+
+        //check to see the weight unit text is now visible
+        onView(withId(R.id.deadLiftWeightTV)).check(matches(isDisplayed()));
+    }
+
+    //test to check that weight tv is displayed after something is entered into weight edit text
+    //in Over Head Press card view
+    @Test
+    public void enterTextIntoOverHeadPressWeightEt(){
+
+        final int WEIGHTTEXT = 60;
+
+        //click the weight Edit Text
+        onView(withId(R.id.overHeadPressWeightET)).perform(click()).check(matches(hasFocus()));
+        //enter text into weight Edit Text
+        onView(withId(R.id.overHeadPressWeightET)).perform(typeText(String.valueOf(WEIGHTTEXT)));
+
+        //check to see the weight unit text is now visible
+        onView(withId(R.id.overHeadPressWeightTV)).check(matches(isDisplayed()));
     }
 }
